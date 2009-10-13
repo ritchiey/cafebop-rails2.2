79c9cbf8f7a254ba298e9cb3d8ad437348bce4ef
Ritchie Young <ritchie@gmail.com>
2009-10-13 16:54:28 +0800

    Achieved a basic layout with blueprint css framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  def doctype_header
+    '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">'
+  end
+
+  def default_content_for(name, &block)
+    name = name.kind_of?(Symbol) ? ":#{name}" : name
+    out = eval("yield #{name}", block.binding)
+    concat(out || capture(&block), block.binding)
+  end
+
+
+  def placeholder
+    image_tag 'transparent.png', :alt=>''
+  end
 end
