e0a3cc9a5028c71efc3753075c4a1360ad27aa8e
Nowhere Man <nowhereman@open_desktop>
2009-10-28 22:03:01 +0100

    Web browsers must support cookies to use Cafebop website.

--- test/functional/shops_controller_test.rb
+++ test/functional/shops_controller_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class ShopsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "existing functional tests" do
+    setup do
+      @request.cookies[SESSION_KEY] = "faux session"
+    end
+
+    should "continue to work" do
+      get :index
+      assert_response :success
+    end
+  end
+  
+  context "cookies are disabled " do
+    setup do
+       @request.cookies.delete SESSION_KEY
+    end
+    
+    should "redirect results" do     
+      get :index
+      assert_redirected_to :controller => :shops, :action => :cookies_test
+    end
   end
 end
