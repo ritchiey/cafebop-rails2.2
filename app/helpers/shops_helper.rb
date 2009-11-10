e17b6683a2889e45e2d18c7c408833a81b607b78
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2009-11-10 16:50:01 +0800

    Gradient now appears for shops without a header image.
    Header appears on edit screen

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,7 +1,7 @@
 module ShopsHelper
 
 def edit_shop_link(shop) 
-  shop.can_edit?(current_user) ? link_to('Edit', edit_shop_path(shop)) : ""
+  shop.can_edit?(current_user) ? ( link_to_unless_current('Edit', edit_shop_path(shop)) {""} ) : ""
 end
 
 end
