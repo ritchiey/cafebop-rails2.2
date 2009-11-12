d3f53020931d124221c3a128216f3f226c77ceda
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2009-11-12 11:02:32 +0800

    turned off email confirmation for test

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,5 +1,5 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
-    Notifications.deliver_activate(user)
+    Notifications.deliver_activate(user) unless RAILS_ENV == 'test'
   end
 end
