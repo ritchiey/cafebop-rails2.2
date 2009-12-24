9b95fead9ec7b83121e5606cddb67cf2fd07dd44
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2009-12-24 11:52:51 +0800

    Still doesn't handle ipn
    Needs to process the raw post data before attaching to the IPN verify request

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -20,7 +20,7 @@ class PaymentNotificationsController < ApplicationController
   def verify_with_paypal                           
     paypal_verify_url = paypal_ipn_verify_url
     logger.info "XXXXXXXXXX Checking: #{paypal_verify_url}"
-    response = paypal_service.request_post(paypal_verify_url, request.env['rack.request.form_vars']) 
+    response = paypal_service.request_post(paypal_verify_url, request.raw_post) 
     logger.info response       
     render(:nothing=>true) unless response.body == 'Confirmed'
   end
