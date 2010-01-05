890689421634aafca77b033ec5c64c137032af38
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2010-01-05 15:56:24 +0800

    Made claim notification email asynchronous

--- app/models/claim_observer.rb
+++ app/models/claim_observer.rb
@@ -1,6 +1,6 @@
 class ClaimObserver < ActiveRecord::Observer
   
   def after_create claim
-    Notifications.deliver_new_claim claim
+    Notifications.send_later(:deliver_new_claim, claim)
   end
 end
