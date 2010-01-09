6d378eee385a964756bdc5db375d48b250e61718
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2010-01-09 08:30:50 +0800

    Started with qttouch; very much a work in progess

---
+++ public/javascripts/mobile.js
@@ -0,0 +1,36 @@
+$.jQTouch({
+    icon: '/images/icon.png',
+    formSelector: '.form',
+    statusBar: 'black-translucent'
+    // preloadImages: [
+    //     ]
+});                               
+
+${'a.login'}.tap(function(e) {
+  var $form = $this.closest("form");
+  return app.login($form);
+});
+
+$('#login form').submit(function(e) {
+  var $form = $(this);
+  return app.login($form)
+});     
+
+var app = {
+  login:function($form) {
+    $.ajax({
+     type: $form.attr('method'),
+     url: $form.attr('action'),
+     dataType:'json',
+     data:$form.serialize(),
+     complete:function(req) {
+       if (req.status == 200 || req.status = 304) {
+         jQT.goBack();
+       } else {
+         alert("There was an error logging in. Try again.");
+       }
+     }
+    });
+    return false;
+  }
+};
