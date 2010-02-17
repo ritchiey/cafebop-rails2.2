a4d79ddca06b21de5a52bc488f71605bef2edf03
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2010-02-17 14:56:29 +0800

    mobile: Getting data for home page

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -5,7 +5,16 @@ class FrontController < ApplicationController
   before_filter :current_user_collections, :only=>[:index]
   
   def index
-    @search = Search.new           
+    @search = Search.new
+    respond_to do |format|
+      format.html        
+      format.iphone
+      format.json do
+        render :json=>{:current_orders=>@current_orders,
+                       :work_contracts=>@work_contracts,
+                       :friendships=>@friendships}
+      end              
+    end                 
   end
   
   def cookies_test
--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -9,95 +9,125 @@ var jQT = $.jQTouch({
 
 
 
-// var app = {
-//   login: function($form) {
-//     $.ajax({
-//      type: $form.attr('method'),
-//      url: $form.attr('action'),
-//      data:$form.serialize(),
-//      complete:function(req) {
-//         if (req.status == 200 && req.responseText != 'false') { 
-//          user = req.responseText;
-//          app.updateFormControls();
-//          jQT.goBack();
-//         } else {
-//           alert("There was an error logging in. Try again.");
-//         }
-//      }
-//     });
-//     return false; 
-//   },
-//   
-//   getContent: function(url, success) {
-//     $.ajax({
-//       type: 'GET',
-//       success: success,
-//       url: url,
-//       // beforeSend: function(xhr) {
-//       //   xhr.setRequestHeader("Accept", "text/javascript");
-//       //   alert(xhr);
-//       //   return true;
-//       // },  
-//       dataType: 'json'
-//     });
-//   },
-//   
-//   submitForm: function(form, onComplete) {
-//     $.ajax({
-//      type: form.attr('method'),
-//      url: form.attr('action'),
-//      data:form.serialize(),
-//      complete: onComplete
-//     });
-//   },
-//   
-//   updateFormControls: function() {
-//     if (user != null && user.length > 0) {
-//       $('#login-button').hide();
-//       $('#logout-button').show();
-//     } else {
-//       $('#login-button').show();
-//       $('#logout-button').hide();
-//     }
-//   } 
-// };         
-// 
-// 
-// $(function() { // on page ready
-//   
-//   $('a.login').tap(function(e) { 
-//     var $form = $(this).closest("form");
-//     return app.login($form);   
-//   });
-// 
-//   $('form#login').submit(function(e) {
-//     var $form = $(this);
-//     return app.login($form)
-//   });  
-//   
-//   $('#logout-button').tap(function(e) {
-//     var $form = $('form#logout');
-//     app.submitForm($form, function(req) {
-//       user = null;
-//       app.updateFormControls();
-//     });
-//   });
-//   
-//   app.updateFormControls();  
-// 
-//   // $('#show-queue').bind('pageAnimationEnd', function(e, info) {
-//   //   // alert('Going '+info.direction);
-//   //   if (info.direction == 'out') return;
-//   //   var order_id = 1; // TODO: pick up this value from the page
-//   //   app.getContent('/orders/'+order_id+'/order_items', function(data) {
-//   //     alert('Got content!!');
-//   //     var $ul = $('ul#orders');
-//   //     $ul.append('<li>Something</li>');
-//   //     jQuery.each(data, function() {
-//   //       $ul.append('<li>'+this.name+'</li>');
-//   //     });
-//   //   });
-//   // }); 
-// 
-// 
-// });
+var app = {
+  login: function($form) {
+    $.ajax({
+     type: $form.attr('method'),
+     url: $form.attr('action'),
+     data:$form.serialize(),
+     complete:function(req) {
+        if (req.status == 200 && req.responseText != 'false') { 
+         user = req.responseText;
+         app.updateFormControls();
+         jQT.goBack();
+        } else {
+          alert("There was an error logging in. Try again.");
+        }
+     }
+    });
+    return false; 
+  },
+  
+  getContent: function(url, success) {
+    $.ajax({
+      type: 'GET',
+      success: success,
+      url: url,
+      // beforeSend: function(xhr) {
+      //   xhr.setRequestHeader("Accept", "text/javascript");
+      //   alert(xhr);
+      //   return true;
+      // },  
+      dataType: 'json'
+    });
+  },
+  
+  loadHome: function() {
+    if (app.isLoggedIn()) {
+      alert('Loading home');
+      app.getContent("/", function(data) {
+        alert('got data');
+      })
+    } else {
+      alert('Not logged in so clearing home!');
+      app.clearHomePage();
+    }
+  },
+  
+  submitForm: function(form, onComplete) {
+    $.ajax({
+     type: form.attr('method'),
+     url: form.attr('action'),
+     data:form.serialize(),
+     complete: onComplete
+    });
+  },
+  
+  clearHomePage: function() {
+    // TODO: Remove all items from the home page
+    alert('clearing home page');
+  },
+  
+  isLoggedIn: function() {
+    return (user != null && user.length > 0);
+  },
+  
+  updateFormControls: function() {
+    if (app.isLoggedIn()) {
+      $('#login-button').hide();
+      $('#logout-button').show();
+    } else {
+      $('#login-button').show();
+      $('#logout-button').hide();
+    }
+  } 
+};         
+
+
+$(function() { // on page ready
+  
+  $('a.login').tap(function(e) { 
+    var $form = $(this).closest("form");
+    return app.login($form);   
+  });
+
+  $('form#login').submit(function(e) {
+    var $form = $(this);
+    return app.login($form);
+  });  
+  
+  $('#logout-button').tap(function(e) {
+    var $form = $('form#logout');
+    app.submitForm($form, function(req) {
+      user = null;
+      app.updateFormControls();
+      app.clearHomePage(); 
+      jQT.goBack('#home');
+    });
+  });
+  
+  app.updateFormControls();  
+
+  $('#home').bind('pageAnimationEnd', function(e, info) {
+     if (info.direction == 'out') return;
+     app.loadHome();
+  });
+  
+  app.loadHome();
+
+  // $('#show-queue').bind('pageAnimationEnd', function(e, info) {
+  //   // alert('Going '+info.direction);
+  //   if (info.direction == 'out') return;
+  //   var order_id = 1; // TODO: pick up this value from the page
+  //   app.getContent('/orders/'+order_id+'/order_items', function(data) {
+  //     alert('Got content!!');
+  //     var $ul = $('ul#orders');
+  //     $ul.append('<li>Something</li>');
+  //     jQuery.each(data, function() {
+  //       $ul.append('<li>'+this.name+'</li>');
+  //     });
+  //   });
+  // }); 
+
+
+});
