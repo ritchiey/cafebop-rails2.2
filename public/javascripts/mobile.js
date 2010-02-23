78b51f93a6b71fe27a94467bc1308d2f4338e2c0
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2010-02-23 15:01:08 +0800

    mobile: refactor

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -14,6 +14,59 @@ var app = {
     return src.replace(/-/g, "_" );
   },
 
+  getContent: function(url, success) {
+    $.ajax({
+      type: 'GET',
+      success: success,
+      url: url, 
+      dataType: 'json'
+    });
+  },
+  
+  submitForm: function(form, options) {
+    options = options || {}  
+    $.ajax({
+     type: options['method'] || form.attr('method'),
+     url: options['url'] || form.attr('action'),
+     data:form.serialize(),
+     complete: options['onComplete'] || function() {},
+     dataType: options['dataType'] || 'json',
+    });
+  },
+
+  loadDynamicPage: function(pageSelector, localObjectName, options) {
+    var titleSelector = pageSelector + ' .title';
+    var listSelector = pageSelector + ' .list';
+    var serverObjectName = options['serverObjectName'] || localObjectName;
+    var serverControllerName = options['serverControllerName'] || (serverObjectName + 's')
+    var getTitle = options['getTitle'] || function(obj) {return obj.name};
+    var entryToHtml = options['entryToHtml'] || function(subObj, subCollectionName) {
+      return app.listLink(subObj.name, 'to-'+subCollectionName, subObj.id);
+    }
+    
+    $(titleSelector).text('Loading...');
+    if (app.isLoggedIn()) {
+      app.getContent("/"+serverControllerName+"/"+app[localObjectName+'_id']+"/", function(data) {
+        var obj = data[serverObjectName]     
+        // Set page title
+        $(titleSelector).text(getTitle(obj));
+        
+        // Populate each list on the page
+        $(pageSelector + ' .item-list').each(function(index, itemList) {
+          var subCollectionName = app.underscore($(itemList).attr('id'));
+          if (subCollectionName == null) return; // todo throw exception here
+          if (obj[subCollectionName] == null)  return; // todo throw exception
+          // alert('obj[subCollectionName] = '+obj[subCollectionName].length);
+          var entries = jQuery.map(obj[subCollectionName], function(subObj) {
+            return entryToHtml(subObj, subCollectionName);
+          });
+          $(itemList).empty();
+          $(itemList).append(entries.join(''));
+        });
+      });
+    }
+  },   
+
   login: function($form) {
     app.submitForm($form, {
       onComplete:function(req) {
@@ -28,6 +81,43 @@ var app = {
     })
   },
 
+  listLink: function(label, a_classes, target_id, options) {
+    options = options || {};
+    var li_classes = options['li_classes'] || 'arrow';
+    return "<li class='"+li_classes+"'>"+
+      "<a class='"+a_classes+"'"+
+      " href='#' target-id='"+target_id+
+      "'>"+label+"</a></li>";
+  },
+
+
+  // Register a static page ('#verb-noun') that may load dynamic data
+  // when displayed. Also hook the tap event on any a.to-verb-noun links
+  // and set an variable app.noun_id to be the value taken from the 'target-id'
+  // attribute of the link.
+  addPage: function(verb, noun, onLoad) {  
+    var pageSelector = '#'+verb+'-'+noun
+    $(function() {app.bindPage(pageSelector, onLoad)});
+    app.bindLink('a.to-'+verb+'-'+noun, pageSelector, function(e) {
+      app[app.underscore(noun)+'_id'] = $(e.target).attr('target-id');
+    });
+  },
+  
+  bindPage: function(pageSelector, onLoad) {
+    $(pageSelector).bind('pageAnimationEnd', function(e, info) {
+       if (info.direction == 'out') return;
+       onLoad();
+    });
+  },
+ 
+  bindLink: function(linkSelector, pageSelector, beforeRender) {
+    $(linkSelector).tap(function(e) {
+      beforeRender(e);
+      jQT.goTo(pageSelector, 'slide');
+      return false;
+    });
+  },
+  
   makeQueuedOrderItem: function($form) {
     app.submitForm($form, {
       onComplete: function(req) {
@@ -41,25 +131,6 @@ var app = {
     return false; 
   },
 
-  
-  listLink: function(label, a_classes, target_id, options) {
-    options = options || {};
-    var li_classes = options['li_classes'] || 'arrow';
-    return "<li class='"+li_classes+"'>"+
-      "<a class='"+a_classes+"'"+
-      " href='#' target-id='"+target_id+
-      "'>"+label+"</a></li>";
-  },
-  
-  getContent: function(url, success) {
-    $.ajax({
-      type: 'GET',
-      success: success,
-      url: url, 
-      dataType: 'json'
-    });
-  },
-  
   loadHome: function() {
     if (app.isLoggedIn()) {
       app.getContent("/", function(data) { 
@@ -111,39 +182,6 @@ var app = {
   },   
 
 
-  loadDynamicPage: function(pageSelector, localObjectName, options) {
-    var titleSelector = pageSelector + ' .title';
-    var listSelector = pageSelector + ' .list';
-    var serverObjectName = options['serverObjectName'] || localObjectName;
-    var serverControllerName = options['serverControllerName'] || (serverObjectName + 's')
-    var getTitle = options['getTitle'] || function(obj) {return obj.name};
-    var entryToHtml = options['entryToHtml'] || function(subObj, subCollectionName) {
-      return app.listLink(subObj.name, 'to-'+subCollectionName, subObj.id);
-    }
-    
-    $(titleSelector).text('Loading...');
-    if (app.isLoggedIn()) {
-      app.getContent("/"+serverControllerName+"/"+app[localObjectName+'_id']+"/", function(data) {
-        var obj = data[serverObjectName]     
-        // Set page title
-        $(titleSelector).text(getTitle(obj));
-        
-        // Populate each list on the page
-        $(pageSelector + ' .item-list').each(function(index, itemList) {
-          var subCollectionName = app.underscore($(itemList).attr('id'));
-          if (subCollectionName == null) return; // todo throw exception here
-          if (obj[subCollectionName] == null)  return; // todo throw exception
-          // alert('obj[subCollectionName] = '+obj[subCollectionName].length);
-          var entries = jQuery.map(obj[subCollectionName], function(subObj) {
-            return entryToHtml(subObj, subCollectionName);
-          });
-          $(itemList).empty();
-          $(itemList).append(entries.join(''));
-        });
-      });
-    }
-  },   
-
   loadShowQueuedOrder: function() {
     app.loadDynamicPage('#show-queued-order', 'queued_order', {
       serverObjectName: 'order',
@@ -166,46 +204,6 @@ var app = {
     })
   },
 
-  
-  bindPage: function(pageSelector, onLoad) {
-    $(pageSelector).bind('pageAnimationEnd', function(e, info) {
-       if (info.direction == 'out') return;
-       onLoad();
-    });
-  },
- 
-  bindLink: function(linkSelector, pageSelector, beforeRender) {
-    $(linkSelector).tap(function(e) {
-      beforeRender(e);
-      jQT.goTo(pageSelector, 'slide');
-      return false;
-    });
-  },
-  
-  // Register a static page ('#verb-noun') that may load dynamic data
-  // when displayed. Also hook the tap event on any a.to-verb-noun links
-  // and set an variable app.noun_id to be the value taken from the 'target-id'
-  // attribute of the link.
-  addPage: function(verb, noun, onLoad) {  
-    var pageSelector = '#'+verb+'-'+noun
-    $(function() {app.bindPage(pageSelector, onLoad)});
-    app.bindLink('a.to-'+verb+'-'+noun, pageSelector, function(e) {
-      app[app.underscore(noun)+'_id'] = $(e.target).attr('target-id');
-    });
-  },
-
-  
-  submitForm: function(form, options) {
-    options = options || {}  
-    $.ajax({
-     type: options['method'] || form.attr('method'),
-     url: options['url'] || form.attr('action'),
-     data:form.serialize(),
-     complete: options['onComplete'] || function() {},
-     dataType: options['dataType'] || 'json',
-    });
-  },
-  
   clearHomePage: function() {
     // TODO: Remove all items from the home page
     alert('clearing home page');
