159433e0e0159ad5ed97b3af80952b01824f9800
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2010-03-29 10:13:32 +0800

    Added tld_size for staging

--- config/initializers/subdomain.rb
+++ config/initializers/subdomain.rb
@@ -1,4 +1,6 @@
 SubdomainFu.tld_size = 1 # sets for current environment
 SubdomainFu.tld_sizes = {:development => 0, # localhost
                         :test => 1,
+                        :staging => 2,
                         :production => (ENV['APPLICATION_DOMAIN'] || '.').count('.')}
+                        
