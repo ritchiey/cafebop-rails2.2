22251c3ab4906c7172228cdeb161c0267eddf9d1
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2010-05-12 12:43:51 +0800

    Can now add and remove flavours in menu editor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -132,7 +132,7 @@ module ApplicationHelper
   
   
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", :class=>'remove-control')
   end
   
   def link_to_add_fields(name, f, association)
