c851ddf17c93f9c7499e03e68b9f7c75518e8f3b
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2010-05-24 18:04:35 +0800

    Put version back in

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-# RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
+# require File.join(File.dirname(__FILE__), 'boot')
 
 APPLICATION_DOMAIN= ENV['APPLICATION_DOMAIN'] || 'worknbills.com'
 SUPPORT_EMAIL = 'support@'+APPLICATION_DOMAIN 
