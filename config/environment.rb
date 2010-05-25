cf04aa60a55777566123976e5e323c152731607b
Ritchie Young <ritchiey@ritchie-youngs-computer.local>
2010-05-25 09:23:26 +0800

    Fixed broken environment

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
-# require File.join(File.dirname(__FILE__), 'boot')
+require File.join(File.dirname(__FILE__), 'boot')
 
 APPLICATION_DOMAIN= ENV['APPLICATION_DOMAIN'] || 'worknbills.com'
 SUPPORT_EMAIL = 'support@'+APPLICATION_DOMAIN 
